62f38887dced08d8a5f320ff6739f74c64903a7a
synbyte <rashnikov2.0@gmail.com>
2022-02-11 13:34:54 +0000

    dfd

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Auct/>
+      <Text style={styles.text}>Opeddn up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
+}
+function Auct() {
+  return (
+    <View>
+      <h1>What are you looking for?</h1>
+      <TextInput style={styles.text}/>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000a11',
     alignItems: 'center',
     justifyContent: 'center',
+    color:'#000',
+  },
+  text: {
+    color: 'aquamarine',
   },
 });
